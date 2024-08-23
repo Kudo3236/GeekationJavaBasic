8d80af70018b06877183aa342cf1db1234ebee42
kudou <kudou@192.168.10.112>
2024-08-24 00:52:27 +0900

    7. 繰り返し処理

---
+++ src/basicSyntax/ForWhile.java
@@ -0,0 +1,29 @@
+package basicSyntax;
+
+public class ForWhile {
+
+	public static void main(String[] args) {
+		for(int i = 1; i <= 100; i++) {
+			if(i % 3 == 0 && i % 5 == 0) {
+				System.out.println("FizzBuzz");
+			}else if(i % 3 == 0) {
+				System.out.println("Fizz");
+			}else if(i % 5 == 0) {
+				System.out.println("Buzz");
+			}else {
+			}
+		}
+	
+		String languages[] = {"Ruby", "Golang", "Java", "Python", "Typescript", "HTML", "Javascript"};
+	
+		for(String language: languages) {
+			if(language == "Java") {
+				System.out.println("現在学習中の言語はJavaです。");
+				continue;
+			}else if(language == "HTML") {
+				System.out.println("HTMLはプログラミング言語ではありません");
+				break;
+			}
+		}
+	}
+}
