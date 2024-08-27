23732129e55bfc9115c39aa3a672483da9dce926
kudou <kudou@PC188>
2024-08-27 20:13:44 +0900

    9. クラス

---
+++ src/basicSyntax/Main.java
@@ -0,0 +1,16 @@
+package basicSyntax;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int age = 24;
+		double height = 169.8;
+		int num1 = 200;
+		int num2 = 300;
+		
+		GeekIntroduction.greeting(age, height);
+		GeekIntroduction.specialSkill(num1, num2);
+
+	}
+
+}
